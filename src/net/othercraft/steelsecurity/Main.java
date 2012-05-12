0ddd5bd39e00de4587f4e58dbd6132697c2212d2
Mike Davidson <mike.davidson@comcast.net>
2012-05-12 01:08:36 -0400

    first real commit hopefully

---
+++ src/net/othercraft/steelsecurity/Main.java
@@ -0,0 +1,14 @@
+package net.othercraft.steelsecurity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	public void onDisable() {
+		
+	}
+
+}

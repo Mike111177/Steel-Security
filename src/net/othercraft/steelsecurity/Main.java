97832e21dd92f51d97256b2f83f372c4fddc34be
Mike Davidson <mike.davidson@comcast.net>
2012-05-13 15:01:33 -0400

    commit

--- src/net/othercraft/steelsecurity/Main.java
+++ src/net/othercraft/steelsecurity/Main.java
@@ -1,39 +1,26 @@
 package net.othercraft.steelsecurity;
 
-import java.util.logging.Logger;
-
 import net.othercraft.steelsecurity.commands.Commands;
 import net.othercraft.steelsecurity.listeners.Events;
 
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	
-	private Logger log = Logger.getLogger("Minecraft");
 	
 	public final Events eve = new Events(this);
 	
 	private Commands myExecutor;
 	
 	public void onEnable() {
-		Config.load();
 		myExecutor = new Commands(this);
 		PluginManager pm = this.getServer().getPluginManager();
 		pm.registerEvents(eve, this);
 		getCommand("sts").setExecutor(myExecutor);
-		this.logMessage("Enabled.");
 	}
 	
 	public void onDisable() {
-		this.logMessage("Disabled.");
 		
-	}
-	
-	public void logMessage(String msg) {
-		PluginDescriptionFile pdFile = this.getDescription();
-		this.log.info("[" + pdFile.getName() + "] " + msg); 
 	}
 
 }

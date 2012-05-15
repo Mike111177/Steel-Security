f3b18357afe3a5ea0f4b71927c2570b1df1eed5f
Mike Davidson <mike.davidson@comcast.net>
2012-05-15 06:27:46 -0400

    created new classes did not have time to work on

---
+++ src/net/othercraft/steelsecurity/antispam/anticaps.java
@@ -0,0 +1,5 @@
+package net.othercraft.steelsecurity.antispam;
+
+public class anticaps {
+
+}
---
+++ src/net/othercraft/steelsecurity/antispam/censoredWordProccess.java
@@ -0,0 +1,12 @@
+package net.othercraft.steelsecurity.antispam;
+
+import org.bukkit.event.player.PlayerChatEvent;
+
+public class censoredWordProccess {
+
+	public static void onSpeak(PlayerChatEvent event) {
+		// TODO use the event and the config to cencore words
+		
+	}
+
+}

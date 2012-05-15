66e60badec555da0ff9ba48ac158fd2075599c02
Mike Davidson <mike.davidson@comcast.net>
2012-05-15 05:20:58 -0400

    added no permission messages

--- src/net/othercraft/steelsecurity/commands/sts.java
+++ src/net/othercraft/steelsecurity/commands/sts.java
@@ -1,14 +1,20 @@
 package net.othercraft.steelsecurity.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class sts {
+	
+	String noperm = (ChatColor.RED + "You don't have permission to do this!");
 
 	public void command(CommandSender sender, String[] split) {
 		if (split.length==0) {
 			if (sender.hasPermission("steelsecurity.commands.sts")){
 				sender.sendMessage("This server is running Steel Security");
 				sender.sendMessage("For a list of commands please type /sts help");
+			}
+			else {
+			sender.sendMessage(noperm);				
 			}
 		}
 		else if (split.length>0) {
@@ -17,10 +23,16 @@ public class sts {
 				stshelp sh = new stshelp();
 				sh.sts(sender, split);
 				}
+				else {
+					sender.sendMessage(noperm);
+				}
 			}
 			if (split[0].equalsIgnoreCase("checkperm")){
 				if (sender.hasPermission("steelsecurity.commands.checkperm")) {
 					CheckPerm.checker(sender, split);
+				}
+				else {
+					sender.sendMessage(noperm);
 				}
 			}
 		}

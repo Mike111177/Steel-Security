b63cdb46779088858d9f1cf2ffbc2af3c5d254a5
Mike Davidson <mike.davidson@comcast.net>
2012-05-17 00:02:10 -0400

    config stuff

--- src/net/othercraft/steelsecurity/listeners/Events.java
+++ src/net/othercraft/steelsecurity/listeners/Events.java
@@ -1,29 +1,17 @@
 package net.othercraft.steelsecurity.listeners;
-
+ 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.*;
-
+ 
 import net.othercraft.steelsecurity.Main;
-
+ 
 public class Events implements Listener {
-	Main plugin;
-	private PlayerChatListener chatListener;
-	 
-	public Events(Main instance) {
-	plugin=instance;
-	}
-	public Events(PlayerChatListener chatListener) {
-	this.chatListener = chatListener;
-	}
-	public
-	@EventHandler
-	public void onPlayerChat(PlayerChatEvent event) {
-	chatListener.onChat(event);
-	}
-	@EventHandler
-	public void onPlayerMove(PlayerMoveEvent event) {
-		PlayerMovementListener.onMove(event);
-	}
-
+    Main plugin;
+    private PlayerChatListener chatListener;
+   
+    public Events(Main instance, PlayerChatListener chatListener) {
+        plugin=instance;
+        this.chatListener = chatListener;
+    }
 }

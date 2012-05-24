d7d2f95319965eff2504ce83aa23b1251e25cd42
Mike Davidson <mike.davidson@comcast.net>
2012-05-24 18:16:21 -0400

    config class

---
+++ src/net/othercraft/steelsecurity/Config.java
@@ -0,0 +1,13 @@
+/**
+ * @Class: Config
+ * @Author: Mike111177
+ * @Purpose: This is a class that will handle every action related to the config
+ * @Extends: JavaPlugin
+ *  */
+package net.othercraft.steelsecurity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Config extends JavaPlugin{
+
+}

e946c0f99b3d76688258469035cec0d5a423c8ad
CorrieKay <corriekay.pinkie@gmail.com>
2012-05-24 14:18:10 -0700

    added interface for quick adding of methods to all executors.

---
+++ src/net/othercraft/steelsecurity/utils/SSCmdExeInterface.java
@@ -0,0 +1,11 @@
+/**
+ * @Interface: SSCmdExeInterface
+ * @Author: CorrieKay
+ * @Purpose: Use this interface to quickly and easily add a method to all command executors.
+ */
+
+package net.othercraft.steelsecurity.utils;
+
+public interface SSCmdExeInterface {
+
+}

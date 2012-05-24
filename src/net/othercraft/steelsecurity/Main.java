c8a13548ec4517abe2044c1243c646051f8f5be4
CorrieKay <corriekay.pinkie@gmail.com>
2012-05-24 14:16:08 -0700

    added stuff for custom event logging.
    Signed-off-by: CorrieKay <corriekay.pinkie@gmail.com>

--- src/net/othercraft/steelsecurity/Main.java
+++ src/net/othercraft/steelsecurity/Main.java
@@ -1,13 +1,19 @@
 package net.othercraft.steelsecurity;
 
+import net.othercraft.steelsecurity.Listener.SSEventHandler;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
 
 	public static Main instance;
 	
+	@SuppressWarnings("unused")
+	private SSEventHandler eventHandler;
+	
 	public void onEnable(){
 		instance = this;
+		eventHandler = new SSEventHandler();
 	}
 	
 	public void onDisable(){

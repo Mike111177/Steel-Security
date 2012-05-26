d206fe7dfcf5a588fec19f33bf7ad8ac24527e5b
Mike Davidson <mike.davidson@comcast.net>
2012-05-26 16:15:59 -0400

    Config defaults and get methods implemented

--- src/net/othercraft/steelsecurity/Config.java
+++ src/net/othercraft/steelsecurity/Config.java
@@ -6,16 +6,39 @@
  *  */
 package net.othercraft.steelsecurity;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Config extends JavaPlugin{
-	
-	public void loadConfiguration(){
-		defaults();
-		//TODO load configuration
-	}
-	public void defaults(){
+public class Config extends JavaPlugin {
 		
+		public void loadConfiguration() {
+			defaults();
+			getConfig().options().copyDefaults(true);
+			saveConfig();
+		}	
+		private void defaults() { //Defaults
+			getConfig().addDefault("General.Prefix", "[STS]");
+			getConfig().addDefault("General.Logon_Message_Enabled", true);
+			getConfig().addDefault("General.Logon_Message", "Steel Security is running on this server.");
+			getConfig().addDefault("AntiSpam.Censoring.Enabled", false );
+			getConfig().addDefault("AntiSpam.Censoring.Block_Words", Arrays.asList("Nodus", "Avo", "ICHG"));
+			getConfig().addDefault("AntiSpam.AntiCaps.Enabled", true);
+			getConfig().addDefault("AntiSpam.AntiCaps.Minimum_Length", 4);
+			getConfig().addDefault("AntiSpam.AntiCaps.Percent", 70);
+		}
+		@SuppressWarnings("unchecked")
+		public List<String> getConfigurationList(String Listpath) {//Gets a list
+			return (List<String>) getConfig().getList(Listpath);
+		}
+		public String getConfigurationString(String Stringpath){//Gets a string
+			return getConfig().getString(Stringpath);
+		}
+		public Boolean getConfigurationBoolean(String Booleanpath) {//gets a boolean
+			return getConfig().getBoolean(Booleanpath);
+		}
+		public int getConfigurationInt(String Intpath) {//gets an int
+			return getConfig().getInt(Intpath);
+		}
 	}
-
-}

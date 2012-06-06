2421c38bd47fc7be74a9b943437cb69990aa9119
Mike Davidson <mike.davidson@comcast.net>
2012-06-06 12:33:05 -0400

    started work on maing customconfigs

--- src/net/othercraft/steelsecurity/Main.java
+++ src/net/othercraft/steelsecurity/Main.java
@@ -1,9 +1,17 @@
 package net.othercraft.steelsecurity;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import net.othercraft.steelsecurity.commands.Sts;
 import net.othercraft.steelsecurity.listeners.ChatFilter;
 import net.othercraft.steelsecurity.listeners.JoinMessage;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
@@ -11,6 +19,9 @@ public class Main extends JavaPlugin{
 	public static Main instance;
 
 	private Sts base;
+	
+	private FileConfiguration customConfig = null;
+	private File customConfigFile = null;
 
 	@SuppressWarnings("unused")
 	private ChatFilter cf;
@@ -30,6 +41,35 @@ public class Main extends JavaPlugin{
 	private void listeners() {//register listeners here
 		cf = new ChatFilter(null, this);
 		jm = new JoinMessage(null, this);
+	}
+	public void reloadCustomConfig() {
+	    if (customConfigFile == null) {
+	    customConfigFile = new File(getDataFolder(), "customConfig.yml");
+	    }
+	    customConfig = YamlConfiguration.loadConfiguration(customConfigFile);
+	 
+	    // Look for defaults in the jar
+	    InputStream defConfigStream = getResource("customConfig.yml");
+	    if (defConfigStream != null) {
+	        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+	        customConfig.setDefaults(defConfig);
+	    }
+	}
+	public FileConfiguration getCustomConfig() {
+	    if (customConfig == null) {
+	        reloadCustomConfig();
+	    }
+	    return customConfig;
+	}
+	public void saveCustomConfig() {
+	    if (customConfig == null || customConfigFile == null) {
+	    return;
+	    }
+	    try {
+	        customConfig.save(customConfigFile);
+	    } catch (IOException ex) {
+	        Logger.getLogger(JavaPlugin.class.getName()).log(Level.SEVERE, "Could not save config to " + customConfigFile, ex);
+	    }
 	}
 	public void onDisable(){
 	}
--- src/net/othercraft/steelsecurity/listeners/PlayerConfigManager.java
+++ src/net/othercraft/steelsecurity/listeners/PlayerConfigManager.java
@@ -5,12 +5,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import net.othercraft.steelsecurity.Main;
 import net.othercraft.steelsecurity.utils.SSCmdExe;
 
 public class PlayerConfigManager extends SSCmdExe {
 	
-	public PlayerConfigManager(String name) {
+	public Main plugin;
+	
+	public PlayerConfigManager(String name, Main instance) {
 		super(name, true);
+		this.plugin = instance;
 	}
 	@EventHandler
 	public void onLogin(PlayerLoginEvent event) {

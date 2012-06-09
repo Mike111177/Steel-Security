7d60667a482b700779b3840022b79505cf240d28
Mike Davidson <mike.davidson@comcast.net>
2012-06-09 13:52:46 -0400

    tyring to get anti spam but right now its antitalk

--- src/net/othercraft/steelsecurity/listeners/ChatFilter.java
+++ src/net/othercraft/steelsecurity/listeners/ChatFilter.java
@@ -1,6 +1,5 @@
 package net.othercraft.steelsecurity.listeners;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +15,8 @@ public class ChatFilter extends SSCmdExe implements Listener {
 
 	public Main plugin;
 
+	Map<String, Long> chattimes = new HashMap<String, Long>();
+
 	public ChatFilter(String name, Main instance) {
 		super(name, true);//true only if its a listener, false if it isnt
 		this.plugin = instance;
@@ -24,62 +25,72 @@ public class ChatFilter extends SSCmdExe implements Listener {
 	@EventHandler
 	public void onPlayerChat(PlayerChatEvent event){
 		try {
+			Boolean spam = true;
+			System.out.println(spam);
 			String name = event.getPlayer().getName();
-			Map<String, Long> chattimes = new HashMap<String, Long>();
-			long time = new Date().getTime();
-			long lasttime = chattimes.get(name);
+			long time = System.currentTimeMillis();
+			Long lasttime = chattimes.get(name);
+			if(lasttime == null)lasttime = Long.valueOf(time - 1000);
+			long check = (time - lasttime);
+			System.out.println(check);
 			chattimes.put(name, time);
-			if (time - lasttime > plugin.getConfig().getInt("AntiSpam.AntiFlood.Speed") || plugin.getConfig().getBoolean("AntiSpam.AntiFlood.Enabled") == false) {
+			if (check > plugin.getConfig().getInt("AntiSpam.AntiFlood.Speed") || plugin.getConfig().getBoolean("AntiSpam.AntiFlood.Enabled") == false) {
+				spam = false;
+				System.out.println(spam);
+			}
+			System.out.println(spam);
 			String message = event.getMessage();
-			if (plugin.getConfig().getBoolean("AntiSpam.Censoring.Enabled") && event.getPlayer().hasPermission("steelsecurity.antispam.bypasscensor") == false) {
-				@SuppressWarnings("unchecked")
-				List<String> list =  (List<String>) plugin.getConfig().getList("AntiSpam.Censoring.Block_Words");
-				int wordcount = list.size();
-				int wordcounter = 0;
-				while (wordcounter<wordcount) {
-					String newword;
-					String badword = list.get(wordcounter);
-					int lettercount = list.get(wordcounter).toCharArray().length;
-					int lettercounter = 0;
-					newword = "";
-					while (lettercounter < lettercount) {
-						newword = (newword + "*");
-						++lettercounter;
+			if (!spam) {
+				System.out.println(spam);
+				if (plugin.getConfig().getBoolean("AntiSpam.Censoring.Enabled") && event.getPlayer().hasPermission("steelsecurity.antispam.bypasscensor") == false) {
+					@SuppressWarnings("unchecked")
+					List<String> list =  (List<String>) plugin.getConfig().getList("AntiSpam.Censoring.Block_Words");
+					int wordcount = list.size();
+					int wordcounter = 0;
+					while (wordcounter<wordcount) {
+						String newword;
+						String badword = list.get(wordcounter);
+						int lettercount = list.get(wordcounter).toCharArray().length;
+						int lettercounter = 0;
+						newword = "";
+						while (lettercounter < lettercount) {
+							newword = (newword + "*");
+							++lettercounter;
+						}
+						message = message.replaceAll("(?i)" + badword, newword);
+						++wordcounter;
 					}
-					message = message.replaceAll("(?i)" + badword, newword);
-					++wordcounter;
 				}
-			}
 
-			if (event.getMessage().length()>plugin.getConfig().getInt("AntiSpam.AntiCaps.Minimum_Length")){
-				if (plugin.getConfig().getBoolean("AntiSpam.AntiCaps.Enabled") && event.getPlayer().hasPermission("steelsecurity.antispam.bypassanticaps") == false) {
-					double percent = plugin.getConfig().getInt("AntiSpam.AntiCaps.Percent");
-					int capcount = message.length();
-					int capcounter = 0;
-					Double uppercase = 0.0;
-					Double lowercase = 0.0;
-					while (capcounter<capcount) {
-						if (message.toCharArray()[capcounter] == message.toLowerCase().toCharArray()[capcounter]) {
-							++lowercase;
+				if (event.getMessage().length()>plugin.getConfig().getInt("AntiSpam.AntiCaps.Minimum_Length")){
+					if (plugin.getConfig().getBoolean("AntiSpam.AntiCaps.Enabled") && event.getPlayer().hasPermission("steelsecurity.antispam.bypassanticaps") == false) {
+						double percent = plugin.getConfig().getInt("AntiSpam.AntiCaps.Percent");
+						int capcount = message.length();
+						int capcounter = 0;
+						Double uppercase = 0.0;
+						Double lowercase = 0.0;
+						while (capcounter<capcount) {
+							if (message.toCharArray()[capcounter] == message.toLowerCase().toCharArray()[capcounter]) {
+								++lowercase;
+							}
+							else {
+								++uppercase;
+							}
+							++capcounter;
 						}
-						else {
-							++uppercase;
+						double total = uppercase + lowercase;
+						double result = uppercase/total;
+						percent = percent/100;
+						if (percent<result){
+							message = message.toLowerCase();
 						}
-						++capcounter;
-					}
-					double total = uppercase + lowercase;
-					double result = uppercase/total;
-					percent = percent/100;
-					if (percent<result){
-						message = message.toLowerCase();
 					}
 				}
+				event.setMessage(message);
+				System.out.println(spam);
 			}
-			event.setMessage(message);
-			}
-			else {
-				event.setCancelled(true);
-			}
+			System.out.println(spam);
+			event.setCancelled(spam);
 		}
 		catch (Exception e){
 			catchListenerException(e, event.getEventName());

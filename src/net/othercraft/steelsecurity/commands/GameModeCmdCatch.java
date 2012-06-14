b26d197745e18ea6b06ed477361b76128801a788
Mike Davidson <mike.davidson@comcast.net>
2012-06-14 11:37:14 -0400

    finished online gamemode changing

--- src/net/othercraft/steelsecurity/commands/GameModeCmdCatch.java
+++ src/net/othercraft/steelsecurity/commands/GameModeCmdCatch.java
@@ -3,7 +3,10 @@ package net.othercraft.steelsecurity.commands;
 import net.othercraft.steelsecurity.Main;
 import net.othercraft.steelsecurity.utils.SSCmdExe;
 
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
@@ -21,6 +24,40 @@ public class GameModeCmdCatch extends SSCmdExe {
 		}
 	}
 	public static void stsgamemode(CommandSender sender, String[] args) {
-		//TODO make this do something lol
+		String usage = "/sts gamemode <player> <Game Mode>";
+		if (args.length<3){
+			sender.sendMessage("Not enough arguments!");
+			sender.sendMessage(usage);
+		}
+		else if (args.length>3) {
+			sender.sendMessage("Too many arguments!");
+			sender.sendMessage(usage);
+		}
+		else {
+			Player target = Bukkit.getPlayer(args[1]);
+			if (target.isOnline()){
+				switch (args[1]){
+				case "1":
+					target.setGameMode(GameMode.getByValue(1));
+					break;
+				case "0":
+					target.setGameMode(GameMode.getByValue(0));
+					break;
+				case "(?i)creative":
+					target.setGameMode(GameMode.getByValue(1));
+					break;
+				case "(?i)survival":
+					target.setGameMode(GameMode.getByValue(0));
+					break;
+				default:
+					sender.sendMessage("Unknown game mode: " + args[1]);
+					break;
+				}
+			}
+			else {
+				
+			}
+
+		}
 	}
 }

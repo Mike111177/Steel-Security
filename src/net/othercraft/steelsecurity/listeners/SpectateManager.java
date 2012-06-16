ec29bfec7aa25ec1530ebc0566ecf99aa5a18987
Mike Davidson <mike.davidson@comcast.net>
2012-06-16 16:15:27 -0400

    started working on /sts spectate

---
+++ src/net/othercraft/steelsecurity/listeners/SpectateManager.java
@@ -0,0 +1,11 @@
+package net.othercraft.steelsecurity.listeners;
+
+import net.othercraft.steelsecurity.utils.SSCmdExe;
+
+public class SpectateManager extends SSCmdExe {
+
+	public SpectateManager(String name, Boolean listener) {
+		super("SpectateManager", true);
+	}
+
+}
